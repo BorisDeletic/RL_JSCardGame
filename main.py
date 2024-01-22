be8c4e895befe275a5c28c1c20628ea6bef22d86
borisdeletic <borisdeletic0@gmail.com>
2024-01-22 13:21:57 +1100

    init commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+
+'''
+This game is inspired by a Jane street problem
+'''
+
+
+class CardGameEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    def __init__(self, render_mode=None):
+        # Observations are dictionaries with the agent's and the target's location.
+        # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
+        self.observation_space = spaces.Dict(
+            {
+                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+
+        # We have 52 actions, corresponding to choosing / not choosing a card
+        self.action_space = spaces.Discrete(52)
+
+        """
+        The following dictionary maps abstract actions from `self.action_space` to
+        the direction we will walk in if that action is taken.
+        I.e. 0 corresponds to "right", 1 to "up" etc.
+        """
+        # self._action_to_direction = {
+        #     0: np.array([1, 0]),
+        #     1: np.array([0, 1]),
+        #     2: np.array([-1, 0]),
+        #     3: np.array([0, -1]),
+        # }
+
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        """
+        If human-rendering is used, `self.window` will be a reference
+        to the window that we draw to. `self.clock` will be a clock that is used
+        to ensure that the environment is rendered at the correct framerate in
+        human-mode. They will remain `None` until human-mode is used for the
+        first time.
+        """
+        self.window = None
+        self.clock = None
